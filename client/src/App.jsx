4e1bf6590b8f57f7e3fa0191d44cc1e98f9b22b6
Ernest Kamau <1.kamauernest@gmail.com>
2025-07-15 15:15:19 +0300

    added vite config

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='bg-amber-800 justify-center'>
+        <p className='text-4xl'>hello</p>
+      </div>
+    </>
+  )
+}
+
+export default App
