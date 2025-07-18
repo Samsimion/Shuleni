8f077122b91fcdc4ecc10b256e39b71a8ce9fc40
Ernest Kamau <1.kamauernest@gmail.com>
2025-07-18 04:33:49 +0300

    adding bcrypt to user model

--- server/models/user.py
+++ server/models/user.py
@@ -1,5 +1,6 @@
 
-from app import db
+
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 from datetime import datetime, timezone
 
@@ -10,7 +11,7 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(255), nullable=False)
+    _password_hash = db.Column('password_hash', db.String(255), nullable=False)
     role = db.Column(db.Enum('owner', 'educator', 'student', name='user_roles'), nullable=False)
     school_id = db.Column(db.Integer, db.ForeignKey('schools.id'), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now(timezone.utc))
@@ -31,14 +32,26 @@ class User(db.Model, SerializerMixin):
     foreign_keys='Submission.graded_by'
 )
     student_profile = db.relationship("Student", back_populates="user", uselist=False)
-
     
-    
-
     messages = db.relationship('Chat', back_populates='sender')
 
     serialize_rules = ('-password_hash', '-school.users', '-class_memberships.user', '-attendances.student', '-marked_attendance.educator')
+    
+    
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError('Password hash may not be viewed.')
+
+    @password_hash.setter
+    def password_hash(self, password):
+        hashed_password = bcrypt.generate_password_hash(password.encode('utf-8'))
+        self._password_hash = hashed_password.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
 
 
     def __repr__(self):
         return f"<User id={self.id} name='{self.full_name}' role={self.role} school_id={self.school_id}>"
+    
+from app import db, bcrypt
