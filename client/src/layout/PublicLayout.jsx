fbfb3761a545571a490cc620cc58688bf9b35981
Ernest Kamau <1.kamauernest@gmail.com>
2025-07-18 07:33:16 +0300

    adds Login page with authentication

--- client/src/layout/PublicLayout.jsx
+++ client/src/layout/PublicLayout.jsx
@@ -1,19 +1,13 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 const PublicLayout = () => {
-    // const { pathname } = useLocation();
-
-    // const showHeader = pathname === '/'
-
     return (
         <div className="flex flex-col min-h-screen">
-            
             <main>
                 <Outlet />
             </main>
-            
         </div>
     );
-}; 
+};
 
 export default PublicLayout;
