f691e1b12325532dc8c04170b1da64b0bf299821
Ernest Kamau <1.kamauernest@gmail.com>
2025-07-18 07:13:23 +0300

    adds axios for api calls

---
+++ client/src/utils/axios.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+// Create an Axios instance with base config
+const instance = axios.create({
+  baseURL: 'http://127.0.0.1:5555', // adjust if using a different port or base
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor to attach token
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+// Optional: Response interceptor for global error handling
+instance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message = error.response?.data?.error || 'Something went wrong';
+    return Promise.reject(new Error(message));
+  }
+);
+
+export default instance;
