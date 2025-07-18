cddf0a374b0ec4363a855a5e361d76ccd0421c6e
Ruth Kiptoo <ruthkiptoo523@gmail.com>
2025-07-18 07:26:50 +0300

    git

--- server/routes/schools.py
+++ server/routes/schools.py
@@ -1,10 +1,12 @@
 from flask_restful import Resource
-from flask_marshmallow import SQLAchemySchema, auto_field
-from models.school import School
+from flask_marshmallow import MarshMallow
+from marshmallow_sqlalchemy import auto_field
+from models.school import School, db
+
 
 class SchoolSchema(ma.SQLAlchemySchema):
     class Meta:
-        model = school
+        model = School
         load_instance = True
 
     id = ma.auto_field()
@@ -12,3 +14,26 @@ class SchoolSchema(ma.SQLAlchemySchema):
     description = auto_field()
     created_at = auto_field()
     owner_id = auto_field()
+
+school_schema = SchoolSchema(many=True)  
+single_school_schema = SchoolSchema()  
+
+class SchoolResource(Resource):
+    def get(self):
+        schools = School.query.all()
+        return school_schema.dump(schools), 200
+
+    def post(self):
+        data = request.get_json()
+        name = data.get("name")
+        description = data.get("description")
+        owner_id = data.get("owner_id")  
+
+        if not name:
+            return {"error": "School name is required"}, 400
+
+        new_school = School(name=name, description=description, owner_id=owner_id)
+        db.session.add(new_school)
+        db.session.commit()
+        return school_schema.dump(new_school), 201      
+
