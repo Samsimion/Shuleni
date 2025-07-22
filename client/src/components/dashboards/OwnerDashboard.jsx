728db8e213ceabdd73ec82c56f68dc05f0d595de
deborahchepkoech <deborah.chepkoech@student.moringaschool.com>
2025-07-22 14:49:36 +0300

    updated

--- client/src/components/dashboards/OwnerDashboard.jsx
+++ client/src/components/dashboards/OwnerDashboard.jsx
@@ -69,7 +69,7 @@ const OwnerDashboard = () => {
             <button onClick={CreateEducatorRedirect} className="block w-full text-left px-3 py-2 rounded hover:bg-gray-100">
               Manage teachers
             </button>
-            <button onClick={CreateEducatorRedirect} className="block w-full text-left px-3 py-2 rounded hover:bg-gray-100">
+            <button className="block w-full text-left px-3 py-2 rounded hover:bg-gray-100">
               Manage schools
             </button>
             <button className="block w-full text-left px-3 py-2 rounded hover:bg-gray-100">
